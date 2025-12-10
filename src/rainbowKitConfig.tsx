e8418a37ce2b72cedf2b52c28930f68bc9410442
OrsiniBr <udochukwugodwin13@gmail.com>
2025-12-10 23:53:53 +0100

    add amoy

--- src/rainbowKitConfig.tsx
+++ src/rainbowKitConfig.tsx
@@ -1,11 +1,11 @@
 "use client"
 
 import { getDefaultConfig } from "@rainbow-me/rainbowkit"
-import { mainnet, polygon, arbitrum, optimism, sepolia } from "wagmi/chains";
+import { mainnet, polygon, arbitrum, optimism, polygonAmoy } from "wagmi/chains";
 
 export default getDefaultConfig({
     appName: "Cross-Credit Lending",
     projectId: process.env.NEXT_PUBLIC_WALLETCONNECT_PROJECT_ID!,
-    chains: [mainnet, polygon, arbitrum, optimism, sepolia],
+    chains: [mainnet, polygon, arbitrum, optimism, polygonAmoy],
     ssr: false,
 })
