e19195bed375904d00e42587da15803a48a4e6f5
OrsiniBr <udochukwugodwin13@gmail.com>
2025-12-16 21:35:54 +0100

    change turbo pack to true

--- dapp/next.config.ts
+++ dapp/next.config.ts
@@ -1,35 +1,10 @@
 import type { NextConfig } from "next";
 
-const nextConfig: NextConfig = {
-  // Remove experimental.turbo - it's causing issues
+/** @type {import('next').NextConfig} */
+const nextConfig = {
   experimental: {
-    // Remove 'turbo' key
+    turbo: true,
   },
-
-  // Add webpack config to exclude test files
-  webpack: (config, { isServer }) => {
-    if (!isServer) {
-      // Exclude problematic node_modules from client bundle
-      config.resolve.fallback = {
-        ...config.resolve.fallback,
-        fs: false,
-        net: false,
-        tls: false,
-        crypto: false,
-      };
-    }
-
-    // Exclude test files from bundle
-    config.module.rules.push({
-      test: /node_modules\/thread-stream\/(test|bench)/,
-      use: "null-loader",
-    });
-
-    return config;
-  },
-
-  // Exclude problematic packages from server components
-  // serverComponentsExternalPackages: ["pino", "pino-pretty", "thread-stream"],
 };
 
-export default nextConfig;
+module.exports = nextConfig;
