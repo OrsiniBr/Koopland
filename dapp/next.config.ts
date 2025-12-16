9bba073eb841cf5bd0b2be9e4021c177341daba2
OrsiniBr <udochukwugodwin13@gmail.com>
2025-12-16 22:34:39 +0100

    mad forking

--- dapp/next.config.ts
+++ dapp/next.config.ts
@@ -1,10 +1,7 @@
 import type { NextConfig } from "next";
 
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-  experimental: {
-    turbo: true,
-  },
+const nextConfig: NextConfig = {
+  /* config options here */
 };
 
-module.exports = nextConfig;
+export default nextConfig;
